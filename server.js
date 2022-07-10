ce6aa58ba582ded7b6dd65b29e9fb084da51e3f3
McNeill Shiner <mcneill.shiner@gmail.com>
2022-07-09 21:57:10 -0400

    set middleware

--- server.js
+++ server.js
@@ -15,3 +15,14 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+app.set('view engine', 'ejs')
+// sets up public folder, where all files will be served from if anyone accesses the base path
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
